1bcc47697ea9c9346a36da7b40b36e7107f35aca
Muhammad Adil <muhammadadeel20300@gmail.com>
2024-03-21 12:02:32 +0500

    USING LOOP

---
+++ app.js
@@ -0,0 +1,26 @@
+// var arr =[1,2,3,'hassan',4,5,6,'Alameen',7,8,9,10,'Adil']
+ 
+// for(var i=0; i<arr.length; i++){
+//     document.write(arr[i]," " ," ")
+// }
+// alert('Enter Any Number ')
+// document.write('<h1> Table  </h1>')
+// var userinp = +prompt('Enter any Number ')
+ 
+// for (var i=1; i<=10; i++){
+    // document.write(userinp,"x",i,"=",userinp*i,"<br>")
+// }
+
+// var arr = ['adil','alameen','hassan','ahmed']
+
+// for( var i=0; i<arr.length; i++){
+//     document.write(arr[i]," ",  )
+// }
+
+alert('Enter any Number ')
+document.write('<h1> Table  </h1>')
+var userinp = +prompt('Enetr any number ')
+
+for(var i=1;i<=10; i++){
+    document.write(userinp,"  ",'x', "  ", i, " ",'=', "  ",userinp*i,"<br>")
+}
